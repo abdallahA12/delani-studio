8cd62f5835b0a7046ac3a111890a6a583fde83ee
Adam Abdallah <khalizatelli@gmail.com>
2019-11-05 10:00:28 +0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
             $("#icon").click(function() {
+                        $(".icon-showing").toggle(slow),
+                            $(".icon-hidden").toggle(slow)
+                    }
+                    $(document).ready(function() {
+                            $("#icon").click(function() {
+                                        $(".icon2-showing").toggle(slow),
+                                            $(".icon2-hidden").toggle(slow)
+                                    }
+                                    $(document).ready(function() {
+                                            $("#icon").click(function() {
+                                                $(".icon3-showing").toggle(slow),
+                                                    $(".icon3-hidden").toggle(slow)
+                                            });
